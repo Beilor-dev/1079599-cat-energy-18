9303970c453ac1cec1bb5fe46a2336215345b430
Beilor-dev <oleg.opc@gmail.com>
2019-12-27 23:32:29 +0300

    npm run build done v.1 + mm v.1

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -2,6 +2,8 @@
 
 var closeButton = document.getElementsByClassName('main-nav__switch')[0];
 var navMenu = document.getElementsByClassName('main-nav__list')[0];
+var mainNav = document.querySelector('.main-nav');
+var mainNavToggle = mainNav.querySelector('.main-nav__switch');
 
 closeButton.classList.add('main-nav__switch_state-shown');
 navMenu.classList.add('main-nav__list_state-hide');
@@ -9,3 +11,12 @@ navMenu.classList.add('main-nav__list_state-hide');
 closeButton.addEventListener('click', function() {
   navMenu.classList.toggle('main-nav__list_state-hide');
 });
+
+mainNavToggle.addEventListener('click', function () {
+
+  if (mainNav.classList.contains('main-nav--close')) {
+    mainNav.classList.remove('main-nav--close');
+  } else {
+    mainNav.classList.add('main-nav--close');
+  }
+})
